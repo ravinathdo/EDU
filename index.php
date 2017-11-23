9336386ecaa3b540fc731bfbbd4462974e07bbba
ravi <ravi@ravi-PC>
2017-11-23 14:18:48 +0530

    Merge origin/master
    Conflicts:
    	_menu_member.php
    	admin_member_registration.php
    	manager_recommond_member.php
    	member_post.php
    	model/UserModel.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!--
-author: W3layouts
-author URL: http://w3layouts.com
-License: Creative Commons Attribution 3.0 Unported
-License URL: http://creativecommons.org/licenses/by/3.0/
+ 
+author : promod
+ 
+ 
 -->
 <!DOCTYPE html>
 <html lang="zxx">
